6fae08119c902139b08f6b127dd91001a413bce9
AdrianaPana <88066167+AdrianaPana@users.noreply.github.com>
2021-10-16 16:18:57 -0600

    Updated link rel

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ export default function App() {
       <h1>Weather App</h1>
       <footer>
         This project was coded by Adriana Pana and is {" "}
-      <a href="https://github.com/AdrianaPana/react-weather-project" target="_blank">      open-sourced on GitHub
+      <a href="https://github.com/AdrianaPana/react-weather-project" target="_blank" rel="noreferrer">      open-sourced on GitHub
 </a>
     </footer>
     </div>
