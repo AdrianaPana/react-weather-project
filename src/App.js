02d9a7fd434ee9f680d414245271fa137fe8fe03
AdrianaPana <88066167+AdrianaPana@users.noreply.github.com>
2021-10-16 15:58:10 -0600

    Added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -5,7 +5,12 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      Weather App
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Adriana Pana and is {" "}
+      <a href="https://github.com/AdrianaPana/react-weather-project" target="_blank">      open-sourced on GitHub
+</a>
+    </footer>
     </div>
   );
 }
