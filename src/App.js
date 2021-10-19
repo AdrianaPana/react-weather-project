2ebada1a39c3c3776800c83c875f25f8f6cdfe72
AdrianaPana <88066167+AdrianaPana@users.noreply.github.com>
2021-10-18 22:02:17 -0600

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by Adriana Pana and is {" "}
-      <a href="https://github.com/AdrianaPana/react-weather-project" target="_blank" rel="noreferrer">      open-sourced on GitHub
-</a>
-    </footer>
+        <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by Adriana Pana and is {" "}
+        <a href="https://github.com/AdrianaPana/react-weather-project" target="_blank" rel="noreferrer">      open-sourced on GitHub
+  </a>
+      </footer>
+      </div>
     </div>
   );
 }
