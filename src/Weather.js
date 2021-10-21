3b3994638a211d6021f03e1a65ce806a9a3e39c5
AdrianaPana <88066167+AdrianaPana@users.noreply.github.com>
2021-10-20 22:22:45 -0600

    Bug fixed

--- src/Weather.js
+++ src/Weather.js
@@ -30,7 +30,7 @@ export default function Weather(props) {
 
     function search() {
          const apiKey = "55138790f70e5a564e372b7d10cce5ca";
-        let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+        let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
         axios.get(apiUrl).then(handleResponse);
     }
 
